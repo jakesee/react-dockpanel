3ddeb56dd85d8bb8203a255919361cc1ee3d2b84
Jake See <jakesee@gmail.com>
2021-12-04 16:00:55 +0800

    added more complete example

--- example/index.tsx
+++ example/index.tsx
@@ -1,7 +1,7 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { DockManager, useDockManager } from './..';
+import { DockLayoutDirection, DockManager, useDockManager } from './..';
 
 const App = () => {
 
@@ -9,11 +9,22 @@ const App = () => {
 
   React.useEffect(() => {
 
+    const form1 = manager.createForm('Properties');
+    const form2 = manager.createForm('Class View');
+    const form3 = manager.createForm('Document');
+
+    const panel1 = manager.createPanel([form1, form2]);
+    const panel2 = manager.createPanel([form3]);
+
+    const splitter = manager.createSplitter(panel1, panel2, DockLayoutDirection.Horizontal);
+
+    manager.setLayout(splitter);
+
   }, []);
 
   return (
     <div>
-      <DockManager dockManager={manager} onRenderForm={ () => 'No Content'}/>
+      <DockManager manager={manager} onRenderForm={ () => 'No Content'}/>
     </div>
   );
 };
