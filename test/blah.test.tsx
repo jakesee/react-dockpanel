071dfd3c845dc8d73c9e729eec7e430ad2ff4fe2
Jake See <jakesee@gmail.com>
2021-12-04 13:30:17 +0800

    fixed test

--- test/blah.test.tsx
+++ test/blah.test.tsx
@@ -1,18 +1,31 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+// import * as ReactDOM from 'react-dom';
+// import userEvent from '@testing-library/user-event';
+import { render, screen } from '@testing-library/react';
 import { DockManager } from '../src/components/DockManager';
 import { useDockManager } from '../src/components/hooks';
 
+const TestApp = () => {
+  const manager = useDockManager();
+  return (
+    <div>
+      <DockManager dockManager={manager} onRenderForm={() => 'No Content'} />
+    </div>
+  );
+};
+
+
 describe('it', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(
-      <DockManager
-        dockManager={useDockManager()}
-        onRenderForm={() => 'Test Content'}
-      />,
-      div
-    );
-    ReactDOM.unmountComponentAtNode(div);
+
+    render(<TestApp />)
+
+    const textNoContent = screen.getByText(/Drop a form here to display/i);
+
+    expect(textNoContent).toBeTruthy();
+
+    // const div = document.createElement('div');
+    // ReactDOM.render(<TestApp />, div);
+    // ReactDOM.unmountComponentAtNode(div);
   });
 });
