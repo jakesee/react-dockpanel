c32fbd7dfaf2df177cce3c1ed121cfc0b3a4f885
Jake See <jakesee@gmail.com>
2021-12-05 22:39:19 +0800

    added missing exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,2 @@
-export { useDockManager, CDockLayoutItem, CDockForm, CDockSplitter, CDockPanel, DockEvent } from './components/hooks';
+export { useDockManager, CDockLayoutItem, CDockForm, CDockSplitter, CDockPanel, DockEvent, DockPosition } from './components/hooks';
 export { DockManager } from './components/DockManager';
