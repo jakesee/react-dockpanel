508b215c71dd4af3f58139697cbe7f2afcf009ac
Jake See <jakesee@gmail.com>
2021-12-06 13:30:23 +0800

    update exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 export {
-    useDockManager,
-    CDockLayoutItem,
-    CDockForm,
-    CDockSplitter,
-    CDockPanel,
-    DockEvent,
-    DockPosition,
-    RenderFormEvent,
-    RenderPanelEvent
+  useDockManager,
+  CDockLayoutItem,
+  CDockForm,
+  CDockSplitter,
+  CDockPanel,
+  DockEvent,
+  DockPosition,
+  RenderFormEvent,
+  RenderPanelEvent,
 } from './components/hooks';
 export { DockManager } from './components/DockManager';
